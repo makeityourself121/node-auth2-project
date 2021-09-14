017446d6821970fc69cfbde0997f6c215fffe73d
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-14 15:46:47 -0700

    clean up comments

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,4 @@
-const { JWT_SECRET } = require('../secrets') // use this secret!
+const { JWT_SECRET } = require('../secrets')
 const jwt = require('jsonwebtoken')
 const User = require('../users/users-model')
 
@@ -20,21 +20,6 @@ const restricted = (req, res, next) => {
     req.decodedJwt = decoded
     next()
   })
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
 }
 
 const only = (role_name) => (req, res, next) => {
@@ -47,16 +32,6 @@ const only = (role_name) => (req, res, next) => {
       message: 'This is not for you',
     })
   }
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": "This is not for you"
-    }
-
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
 }
 const checkUsernameExists = async (req, res, next) => {
   try {
@@ -70,18 +45,11 @@ const checkUsernameExists = async (req, res, next) => {
   } catch (err) {
     next(err)
   }
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": "Invalid credentials"
-    }
-  */
 }
 
 const validateRoleName = (req, res, next) => {
   let { role_name } = req.body
-  // const role_name = req.body.role_name.trim()
+
   if (role_name) {
     role_name = req.body.role_name.trim()
   }
@@ -99,25 +67,6 @@ const validateRoleName = (req, res, next) => {
     req.role_name = role_name
     next()
   }
-
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
-
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
 }
 
 module.exports = {
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware')
-// const { JWT_SECRET } = require('../secrets') // use this secret!
+
 const bcrypt = require('bcryptjs')
 
 const Users = require('../users/users-model')
@@ -17,17 +17,6 @@ router.post('/register', validateRoleName, (req, res, next) => {
       res.status(201).json(newUser)
     })
     .catch(next)
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
 })
 
 router.post('/login', checkUsernameExists, (req, res, next) => {
@@ -44,25 +33,6 @@ router.post('/login', checkUsernameExists, (req, res, next) => {
       next({ status: 401, message: 'Invalid credentials' })
     }
   })
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
 })
 
 module.exports = router
